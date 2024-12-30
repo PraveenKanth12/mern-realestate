ba6ebb233e59d7121e0a787beb08da7625159148
janjalapraveenkanth12 <janjalapraveenkanth12@gmail.com>
2024-12-30 11:34:21 +0530

    created the routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className='text-green-500'>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-out' element={<SignOut/>} />
+      <Route path='/profile' element={<Profile/>} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
