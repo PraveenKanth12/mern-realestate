36c5674c83e4120214774f48b6ad8674caf38388
janjalapraveenkanth12 <janjalapraveenkanth12@gmail.com>
2025-01-01 15:59:09 +0530

    created a MongoDB

--- APIs/index.js
+++ APIs/index.js
@@ -1,4 +1,15 @@
 const express= require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+const uri = "mongodb+srv://PraveenKanth:Praveen%40123@estateproject.uvogt.mongodb.net/?retryWrites=true&w=majority&appName=estateproject";
+
+mongoose.connect(uri).then(()=>{
+  console.log("DB Connected");
+}).catch((err)=>{
+  console.log(err);
+})
 
 const app=express();
 
